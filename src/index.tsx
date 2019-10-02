06a5e1495680db11b2915fd198f9fd9ba903b265
Mitchell Mosure <hctimit@gmail.com>
2019-10-02 12:36:05 -0500

    refactor: structure changes

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { hydrate, render } from 'react-dom';
 import './index.css';
-import App from './App';
+import { App } from './app';
 import * as serviceWorker from './serviceWorker';
 
 const rootElement = document.getElementById("root");
@@ -14,4 +14,4 @@ if (rootElement && rootElement.hasChildNodes()) {
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
