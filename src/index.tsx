149cbd0f3fd381c175c511ae81708b471ac16976
Mitchell Mosure <hctimit@gmail.com>
2019-10-02 20:00:38 -0500

    fix: minimalcss postbuild error

--- src/index.tsx
+++ src/index.tsx
@@ -5,13 +5,13 @@ import { App } from './app';
 import * as serviceWorker from './serviceWorker';
 
 const rootElement = document.getElementById('root');
-if (rootElement && rootElement.hasChildNodes()) {
+if (rootElement!.hasChildNodes()) {
     hydrate(<App/>, rootElement);
+
+    // If you want your app to work offline and load faster, you can change
+    // unregister() to register() below. Note this comes with some pitfalls.
+    // Learn more about service workers: https://bit.ly/CRA-PWA
+    serviceWorker.register();
 } else {
     render(<App/>, rootElement);
 }
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
