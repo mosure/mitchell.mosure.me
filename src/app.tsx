599e1f6df7d655b6442b015a0ee1d3b791c6e986
Mitchell Mosure <hctimit@gmail.com>
2019-10-02 19:57:20 -0500

    fix: proper init of react router dom

--- src/app.tsx
+++ src/app.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
-import { Redirect, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 
 import { Home } from './home';
 import { Projects } from './projects';
 
 export const App: React.FC = () => {
     return (
-        <Switch>
-            <Route exact path='/' component={Home}/>
-            <Route path='/projects' component={Projects}/>
+        <>
+            <BrowserRouter>
+                <Switch>
+                    <Route exact path='/' component={Home}/>
+                    <Route path='/projects' component={Projects}/>
 
-            <Redirect to='/'/>
-        </Switch>
+                    <Redirect to='/'/>
+                </Switch>
+            </BrowserRouter>
+        </>
     );
 };
