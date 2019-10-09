e4528b26101a3426f7cce0ec168dd5b902aebae0
Mitchell Mosure <hctimit@gmail.com>
2019-10-09 02:03:39 -0500

    feat: added icon links in footer

--- src/home/footer.tsx
+++ src/home/footer.tsx
@@ -1,17 +1,24 @@
 import React from 'react';
-import { Grid, makeStyles, createStyles, Typography } from '@material-ui/core';
+import { Grid, makeStyles, createStyles, Typography, Link, IconButton } from '@material-ui/core';
 
 import { globals } from '../data';
+import { GitHubIcon, LinkedInIcon } from '../shared';
 
 const useStyles = makeStyles((theme) =>
     createStyles({
         footer: {
-            height: '72px',
+            height: '96px',
             backgroundColor: theme.palette.primary.dark,
         },
         consoleFont: {
             fontFamily: '"SF Mono", "Roboto Mono", "Lucida Console", Monaco, monospace, sans-serif',
-            fontSize: '0.75rem',
+            fontSize: '0.86rem',
+        },
+        iconContainer: {
+            paddingTop: '8px',
+        },
+        primaryText: {
+            fill: theme.palette.text.hint,
         },
     }),
 );
@@ -32,6 +39,14 @@ const Footer: React.FC = () => {
                 <Typography className={classes.consoleFont} variant='body2'>
                     Built by {globals.firstName} {globals.lastName}
                 </Typography>
+            </Grid>
+            <Grid item>
+                <IconButton size='small' href={globals.githubUrl} target='_blank' color='secondary'>
+                    <GitHubIcon className={classes.primaryText}/>
+                </IconButton>
+                <IconButton size='small' href={globals.linkedinUrl} target='_blank' color='secondary'>
+                    <LinkedInIcon className={classes.primaryText}/>
+                </IconButton>
             </Grid>
         </Grid>
     );
--- src/shared/svg/index.ts
+++ src/shared/svg/index.ts
@@ -1,2 +1,4 @@
+export * from './github.icon';
+export * from './linkedin.icon';
 export * from './logo-animated';
 export * from './logo-static';
