6ad07acffb5eac0b6d3e62af14cdfe3b067cc41c
Mitchell Mosure <hctimit@gmail.com>
2019-10-10 14:56:51 -0500

    feat: started working on project layouts

--- src/home/experiences.tsx
+++ src/home/experiences.tsx
@@ -45,7 +45,7 @@ const ExperienceItem: React.FC<Experience> = (experience: Experience) => {
     return (
         <div className={classes.experienceContainer}>
             <Grid container spacing={3} justify='center' alignItems='center'>
-                <Grid sm={12} md={4} item>
+                <Grid xs={12} md={4} item>
                     <Grid container className={classes.imageContainer} justify='center' alignItems='center'>
                         <Grid item>
                             <ImageViewer {...experience.image}/>
@@ -53,7 +53,7 @@ const ExperienceItem: React.FC<Experience> = (experience: Experience) => {
                     </Grid>
                 </Grid>
 
-                <Grid sm={12} md={8} item>
+                <Grid xs={12} md={8} item>
                     <Link color='secondary' href={experience.url} target='_blank'>
                         <Typography variant='h1' className={classes.organization}>
                             {experience.organization}
--- src/home/projects.tsx
+++ src/home/projects.tsx
@@ -1,11 +1,54 @@
 import React from 'react';
+import { makeStyles, createStyles, Grid, Card, CardContent, CardActions, Typography } from '@material-ui/core';
 
-import { withFade } from '../shared';
+import { withFade, Project } from '../shared';
 import { HomeItemContainer } from './home-item-container';
+import { projects } from '../data';
+
+const useStyles = makeStyles(() =>
+    createStyles({
+        container: {
+            paddingTop: '48px',
+        },
+        name: {
+            fontSize: '2em',
+            paddingBottom: '4px',
+        },
+    }),
+);
+
+const ProjectItem: React.FC<Project> = (project: Project) => {
+    const classes = useStyles();
+
+    return (
+        <Grid item xs={12} sm={6} lg={4}>
+            <Card>
+                <CardContent>
+                    <Typography variant='h2' className={classes.name} color='secondary'>
+                        {project.name}
+                    </Typography>
+                </CardContent>
+                <CardActions/>
+            </Card>
+        </Grid>
+    );
+};
 
 const Projects: React.FC = () => {
+    const classes = useStyles();
+
     return (
-        <HomeItemContainer header='Projects'/>
+        <HomeItemContainer header='Projects'>
+            <Grid container spacing={4} className={classes.container}>
+                {
+                    projects.map((project, index) => {
+                        return (
+                            <ProjectItem key={index} {...project}/>
+                        );
+                    })
+                }
+            </Grid>
+        </HomeItemContainer>
     );
 };
 
